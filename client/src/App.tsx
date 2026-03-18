236515e6e5a6c243ebb7e5c54700a77356ed53f5
Adam Manning <adam.manning@pro-serveinc.com>
2026-03-18 15:34:37 +0000

    fix: add QueryClientProvider to prevent blank page crash
    AgentFeedbackSection and SkillCard use useQuery from @tanstack/react-query
    but App.tsx was missing QueryClientProvider wrapper. This caused the entire
    React tree to crash with 'No QueryClient set' error, resulting in a blank page.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
 import { Switch, Route, Router } from "wouter";
 import { useHashLocation } from "wouter/use-hash-location";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Layout } from "@/components/Layout";
@@ -26,12 +28,14 @@ function AppRoutes() {
 
 function App() {
   return (
-    <ThemeProvider>
-      <Router hook={useHashLocation}>
-        <AppRoutes />
-      </Router>
-      <Toaster />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <Router hook={useHashLocation}>
+          <AppRoutes />
+        </Router>
+        <Toaster />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
